3c314faa808e829aa09cc7835f0ad4560ccc2ec3
Max Sallam <maximus.sallam@earthsoft.com>
2021-03-30 20:24:24 +0800

    Started video 10.

--- 08_HeaderBar.py
+++ 08_HeaderBar.py
@@ -7,6 +7,28 @@ class MainWindow(Gtk.Window):
         Gtk.Window.__init__(self)
         self.set_border_width(10)
         self.set_default_size(500, 400)
+        self.notebook = Gtk.Notebook()
+        self.add(self.notebook)
+
+        # First page
+        self.page1 = Gtk.Box()
+        self.page1.set_border_width(10)
+        self.page1.add(Gtk.Label("Page 1."))
+        self.notebook.append_page(self.page1, Gtk.Label("First Tab"))
+
+        # Second page
+        self.page2 = Gtk.Box()
+        self.page2.set_border_width(10)
+        self.page2.add(Gtk.Label("Page 2."))
+        image = Gtk.Image.new_from_icon_name("dialog-information-symbolic.symbolic", Gtk.IconSize.MENU)
+        self.notebook.append_page(self.page2, image)
+
+        # Second page
+        self.page2 = Gtk.Box()
+        self.page2.set_border_width(10)
+        self.page2.add(Gtk.Label("Page 3."))
+        image = Gtk.Image.new_from_icon_name("open-menu-symbolic", Gtk.IconSize.MENU)
+        self.notebook.append_page(self.page2, image)
 
         header_bar = Gtk.HeaderBar()
         header_bar.set_show_close_button(True)
--- 09_NotebookTabbedLayout.py
+++ 09_NotebookTabbedLayout.py
@@ -8,6 +8,29 @@ class MainWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="Notebook Demo")
         self.set_border_width(10)
+        self.set_default_size(500, 400)
+        self.notebook = Gtk.Notebook()
+        self.add(self.notebook)
+
+        # First page
+        self.page1 = Gtk.Box()
+        self.page1.set_border_width(10)
+        self.page1.add(Gtk.Label("Page 1."))
+        self.notebook.append_page(self.page1, Gtk.Label("First Tab"))
+
+        # Second page
+        self.page2 = Gtk.Box()
+        self.page2.set_border_width(10)
+        self.page2.add(Gtk.Label("Page 2."))
+        image = Gtk.Image.new_from_icon_name("dialog-information-symbolic.symbolic", Gtk.IconSize.MENU)
+        self.notebook.append_page(self.page2, image)
+
+        # Second page
+        self.page2 = Gtk.Box()
+        self.page2.set_border_width(10)
+        self.page2.add(Gtk.Label("Page 3."))
+        image = Gtk.Image.new_from_icon_name("open-menu-symbolic", Gtk.IconSize.MENU)
+        self.notebook.append_page(self.page2, image)
 
 
 
