13de43ad73bb6f4fc6062c0af297a64f75b6c34f
Max Sallam <maximus.sallam@earthsoft.com>
2021-03-30 19:49:08 +0800

    added latest file.

---
+++ 08_HeaderBar.py
@@ -0,0 +1,51 @@
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk, Gio
+class MainWindow(Gtk.Window):
+
+    def __init__(self):
+        Gtk.Window.__init__(self)
+        self.set_border_width(10)
+        self.set_default_size(500, 400)
+
+        header_bar = Gtk.HeaderBar()
+        header_bar.set_show_close_button(True)
+        header_bar.props.title = "Max's Python Application"
+        self.set_titlebar(header_bar)
+
+        # Dialog button on right
+        dialog_button = Gtk.Button()
+        dialog_icon = Gio.ThemedIcon(name="dialog-information-symbolic.symbolic")
+        dialog_image = Gtk.Image.new_from_gicon(dialog_icon, Gtk.IconSize.BUTTON)
+        dialog_button.add(dialog_image)
+        header_bar.pack_end(dialog_button)
+
+        # Menu button on right
+        menu_button = Gtk.Button()
+        menu_icon = Gio.ThemedIcon(name="open-menu-symbolic")
+        menu_image = Gtk.Image.new_from_gicon(menu_icon, Gtk.IconSize.BUTTON)
+        menu_button.add(menu_image)
+        header_bar.pack_end(menu_button)
+
+        # Create
+        box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        Gtk.StyleContext.add_class(box.get_style_context(), "linked")
+
+        # Left arrow on left
+        left_arrow = Gtk.Button()
+        left_arrow.add(Gtk.Arrow(Gtk.ArrowType.LEFT,Gtk.ShadowType.NONE))
+        box.add(left_arrow)
+
+        # Right arrow on left
+        right_arrow = Gtk.Button()
+        right_arrow.add(Gtk.Arrow(Gtk.ArrowType.RIGHT,Gtk.ShadowType.NONE))
+        box.add(right_arrow)
+
+        # Text input box
+        header_bar.pack_start(box)
+        self.add(Gtk.TextView())
+
+window = MainWindow()
+window.connect("delete-event", Gtk.main_quit)
+window.show_all()
+Gtk.main()
